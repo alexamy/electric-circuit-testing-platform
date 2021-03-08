5ea70c31f966b20e7c4eff3c17258b72448ccd45
Alexander Chaschin <chaschin@uchi.ru>
2021-03-09 00:15:47 +0300

    refactor: split formula_parser private code

--- app/services/formula_parser.rb
+++ app/services/formula_parser.rb
@@ -11,6 +11,7 @@ class FormulaParser < ApplicationService
 
     @text = text
     @calculator = Dentaku::Calculator.new(case_sensitive: true)
+    @entries = text.split("\n").map { |line| parse_one(line) }
 
     parse
   end
@@ -19,19 +20,29 @@ class FormulaParser < ApplicationService
 
   private
 
-  attr_reader :calculator
-
-  def parse
-    entries = text.split("\n").map { |line| parse_one(line) }
-
-    @target = entries.last[:target]
-    @dependencies = entries.reverse.reduce([]) { |result, other| result + other[:dependencies] - [other[:target]] }
-    @bodies = entries.map { |h| { h[:target] => h[:body] } }.reduce(&:merge).symbolize_keys
-  end
+  attr_reader :calculator, :entries
 
   def parse_one(line)
     target, body = line.split('=')
     dependencies = calculator.dependencies(body)
-    { body: body, target: target, dependencies: dependencies }
+    { target: target, body: body, dependencies: dependencies }
+  end
+
+  def parse
+    find_target
+    find_dependencies
+    find_bodies
+  end
+
+  def find_target
+    @target = entries.last[:target]
+  end
+
+  def find_dependencies
+    @dependencies = entries.reverse.reduce([]) { |result, other| result + other[:dependencies] - [other[:target]] }
+  end
+
+  def find_bodies
+    @bodies = entries.map { |entry| [entry[:target], entry[:body]] }.to_h.symbolize_keys
   end
 end
