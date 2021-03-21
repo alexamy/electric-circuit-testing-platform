d15d75344c91e43c9864be33002ac6c108bc0ae6
Alexander Chaschin <chaschin@uchi.ru>
2021-03-21 19:23:48 +0300

    test: add create question

--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -10,8 +10,30 @@ feature "User can create question", "
   given(:user) { create(:user) }
   given(:admin) { create(:user, :admin) }
 
-  scenario "Unauthenticated user cant create question" do
-    visit new_question_path
-    expect(page).to have_current_path(root_path)
+  scenario "Unauthenticated user cant create question"
+
+  scenario "Admin creates question" do
+    visit new_admin_question_path
+
+    fill_in "Текст", with: "Вычислить показание вольтметра XMM1"
+    fill_in "Формула", with: "V=R2/(R1+R2)"
+
+    click_on "Проверить"
+
+    within '.parameter[data-target="R1"]' do
+      fill_in "Мин", with: "100"
+      fill_in "Макс", with: "1000000"
+      fill_in "Шаг", with: "100"
+    end
+
+    within '.parameter[data-target="R2"]' do
+      fill_in "Мин", with: "100"
+      fill_in "Макс", with: "1000000"
+      fill_in "Шаг", with: "100"
+    end
+
+    click_on "Создать"
+
+    expect(page).to have_content "Вопрос успешно создан."
   end
 end
