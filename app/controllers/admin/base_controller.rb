e7e4b637e6bf052420877fa2df59552cd262fe37
Alexander Chaschin <chaschin@uchi.ru>
2021-04-03 14:54:01 +0300

    feat: add user type check in admin namespace

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class Admin::BaseController < ApplicationController
+  layout "admin"
+
+  before_action :admin_required!
+
+  def admin_required!
+    redirect_to root_path, alert: t(".restricted") unless current_user&.admin?
+  end
 end
--- spec/controllers/admin/base_controller_spec.rb
+++ spec/controllers/admin/base_controller_spec.rb
@@ -3,6 +3,33 @@
 require "rails_helper"
 
 RSpec.describe Admin::BaseController, type: :controller do
-  it "restrict access for unauthorized user"
-  it "restrict access for student"
+  let(:user) { create(:user) }
+  let(:admin) { create(:admin) }
+
+  controller do
+    def index
+      head :ok
+    end
+  end
+
+  it "restrict access for unauthorized user" do
+    allow(controller).to receive(:current_user).and_return(nil)
+    get :index
+
+    expect(response).to redirect_to root_path
+  end
+
+  it "restrict access for student" do
+    allow(controller).to receive(:current_user).and_return(user)
+    get :index
+
+    expect(response).to redirect_to root_path
+  end
+
+  it "allow access for admin" do
+    allow(controller).to receive(:current_user).and_return(admin)
+    get :index
+
+    expect(response).to be_successful
+  end
 end
