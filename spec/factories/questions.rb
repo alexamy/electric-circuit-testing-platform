c4f3834a62bcefcda8089364c975142197d0c5ce
Alexander Chaschin <chaschin@uchi.ru>
2021-04-07 02:12:47 +0300

    test: add formula parameters in question factory

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -2,11 +2,17 @@
 
 FactoryBot.define do
   factory :question do
-    formula_text { "V=R" }
+    formula_text { "V=I*R" }
     text { "Найти V" }
     comment { "Comment" }
     precision { 0 }
-    formula { { target: "V", dependencies: %w[R], bodies: { V: "R" } } }
+    formula { { target: "V", dependencies: %w[I R], bodies: { V: "I*R" } } }
     category
+
+    after(:create) do |question|
+      question.formula["dependencies"].each do |dependency|
+        create(:formula_parameter, name: dependency, question: question)
+      end
+    end
   end
 end
