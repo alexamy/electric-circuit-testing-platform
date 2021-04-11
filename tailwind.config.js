dbe369fbb0317d023a43077787b557c9579657cb
Alexander Chaschin <chaschin@uchi.ru>
2021-04-12 01:16:03 +0300

    chore: add tailwind and postcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './app/views/**/*.html.slim',
+    './app/packs/**/*.{js,jsx,ts,tsx,css}'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
