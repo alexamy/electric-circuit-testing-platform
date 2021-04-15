f837dbb21df5410d16e3f70cb15e122b905bc576
Alexander Chaschin <chaschin@uchi.ru>
2021-04-16 01:20:59 +0300

    refactor: extract create_static_question

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,15 +10,18 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @question = Question.find(random_question_id)
-    @static_question = StaticQuestion.new_from(@question)
-    @static_question.user = current_user
-
-    @static_question.save
+    create_static_question
   end
 
   private
 
   def random_question_id
     Question.where(category: @category).pluck(:id).sample
+  end
+
+  def create_static_question
+    @static_question = StaticQuestion.new_from(@question)
+    @static_question.user = current_user
+    @static_question.save
   end
 end
