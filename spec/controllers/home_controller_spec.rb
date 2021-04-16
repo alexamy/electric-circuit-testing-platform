4cf7fc5a9faf3db3856d067639e8d99312f60f14
Alexander Chaschin <chaschin@uchi.ru>
2021-04-16 19:22:29 +0300

    feat: root redirect to tests

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe HomeController, type: :controller do
   describe 'GET #index' do
     it 'renders index view' do
       get :index
-      expect(response).to render_template :index
+      expect(response).to redirect_to tests_url
     end
   end
 end
