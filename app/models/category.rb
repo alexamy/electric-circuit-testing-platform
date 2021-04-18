6d7ba385361d09617c65af62edea265a98ede6be
Alexander Chaschin <chaschin@uchi.ru>
2021-04-19 02:37:16 +0300

    feat: add score_of for category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,20 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
+  CORRECT_SCORE = 1
+  WRONG_SCORE = -2
+
   validates :name, :target_score, presence: true
 
   has_many :questions, dependent: :destroy
+
+  def score_of(user)
+    correct, wrong = StaticQuestion.joins(:question)
+                                   .where(author: user, questions: { category: self })
+                                   .select(:answer, :user_answer)
+                                   .partition { |q| q.answer == q.user_answer }
+                                   .map(&:count)
+
+    correct * CORRECT_SCORE + wrong * WRONG_SCORE
+  end
 end
