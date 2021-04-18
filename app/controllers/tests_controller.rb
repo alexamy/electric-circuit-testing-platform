3bb09938047d501c99e6ff36e7266fb6aff09626
Alexander Chaschin <chaschin@uchi.ru>
2021-04-18 13:57:56 +0300

    refactor: extract redirect_after_answer

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -13,21 +13,12 @@ class TestsController < ApplicationController
     create_static_question
   end
 
-  # NOTE
-  # dont use `redirect_to test_path(@static_question.question.category)`, because
-  # in the future test might be the collection of questions from any categories
   def answer
     @static_question = StaticQuestion.find(params[:id])
     head :forbidden and return unless current_user.author_of?(@static_question)
 
     @static_question.update(user_answer: params[:answer])
-
-    if params[:send_and_quit]
-      redirect_to tests_path, notice: t('.test_end')
-    else
-      @category = Category.find(params[:test_id])
-      redirect_to test_path(@category)
-    end
+    redirect_after_answer
   end
 
   private
@@ -39,5 +30,17 @@ class TestsController < ApplicationController
   def create_static_question
     @static_question = StaticQuestion.new_from(@question)
     @static_question.update(author: current_user)
+  end
+
+  # NOTE
+  # dont use `redirect_to test_path(@static_question.question.category)`, because
+  # in the future *test* might be the collection of questions from any categories
+  def redirect_after_answer
+    if params[:send_and_quit]
+      redirect_to tests_path, notice: t('.test_end')
+    else
+      @category = Category.find(params[:test_id])
+      redirect_to test_path(@category)
+    end
   end
 end
