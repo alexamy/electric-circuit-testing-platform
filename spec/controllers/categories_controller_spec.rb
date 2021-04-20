065855aa4c6c1efbf4331e238b0e07064038f633
Alexander Chaschin <chaschin@uchi.ru>
2021-04-20 19:12:11 +0300

    fix: rubocop warning

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -4,7 +4,6 @@ require 'rails_helper'
 
 RSpec.describe CategoriesController, type: :controller do
   let(:category) { create(:category) }
-  let!(:question) { create(:question, category: category) }
 
   describe 'GET #index_with_questions' do
     before { get :index_with_questions }
