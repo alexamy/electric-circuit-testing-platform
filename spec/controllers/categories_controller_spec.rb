f3b084d4c47a5e9b6ce5c71024c03202a8b73981
Alexander Chaschin <chaschin@uchi.ru>
2021-04-20 20:27:56 +0300

    test: categories index fix

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 
 RSpec.describe CategoriesController, type: :controller do
   let(:category) { create(:category) }
+  let!(:question) { create(:question, category: category) } # rubocop:disable RSpec/LetSetup
 
   describe 'GET #index_with_questions' do
     before { get :index_with_questions }
