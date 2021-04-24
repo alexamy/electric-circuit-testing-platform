c65678794b7bde9618baed034240d5c2b1738537
Alexander Chaschin <chaschin@uchi.ru>
2021-04-24 12:07:59 +0300

    fix: migration foreign key

--- db/migrate/20210414073832_add_columns_to_questions_and_static_questions.rb
+++ db/migrate/20210414073832_add_columns_to_questions_and_static_questions.rb
@@ -4,7 +4,7 @@ class AddColumnsToQuestionsAndStaticQuestions < ActiveRecord::Migration[6.0]
   def change
     change_table :static_questions, bulk: true do |t|
       t.float :user_answer
-      t.belongs_to :author, null: false, foreign_key: true
+      t.belongs_to :author, null: false, foreign_key: { to_table: :users }
     end
 
     change_table :questions do |t|
