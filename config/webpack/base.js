1134c8e6899cee717f6c54e1562e8ab62d95d685
Alexander Chaschin <chaschin@uchi.ru>
2021-04-24 21:40:17 +0300

    chore: add remix icons pack

--- config/webpack/base.js
+++ config/webpack/base.js
@@ -1,4 +1,13 @@
 const { webpackConfig, merge } = require('@rails/webpacker')
 const customConfig = require('./custom')
 
+webpackConfig.module.rules.map(module => { // lets iterate Rules...
+  if (module.test && module.test.toString().includes('css')) {
+    module.use.splice(-1, 0, {
+      loader: require.resolve('resolve-url-loader')
+    })
+  }
+  return module;
+});
+
 module.exports = merge(webpackConfig, customConfig)
