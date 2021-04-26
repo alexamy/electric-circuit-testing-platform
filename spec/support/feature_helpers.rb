1a00ecc98ecfed5bb4e89659cdfd8746d99e2cc6
Alexander Chaschin <chaschin@uchi.ru>
2021-04-26 08:51:57 +0300

    test: fix login helper

--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -3,8 +3,11 @@
 module FeatureHelpers
   def sign_in(user)
     visit new_user_session_path
-    fill_in 'Email', with: user.email
-    fill_in 'Пароль', with: user.password
-    click_on 'Войти'
+
+    within '.new_user' do
+      fill_in 'Email', with: user.email
+      fill_in 'Пароль', with: user.password
+      click_on 'Войти'
+    end
   end
 end
