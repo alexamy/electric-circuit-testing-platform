6fc69b39d52104d370219910355e91bef246eb4b
Alexander Chaschin <chaschin@uchi.ru>
2021-05-01 09:50:32 +0300

    fix: seed helpers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 # users
-User.find_or_create_by(email: 'user@test.com') do |user|
-  user.password = 'user'
+User.find_or_create_by!(email: 'user@test.com') do |user|
+  user.password = 'user123'
 end
 
-Admin.find_or_create_by(email: 'admin@test.com') do |user|
-  user.password = Rails.env.ADMIN_PASSWORD
+if Rails.env.development?
+  Admin.find_or_create_by!(email: 'admin@test.com') do |user|
+    user.password = 'testtest'
+  end
 end
 
 require Rails.root.join('db/seeds/questions/main')
