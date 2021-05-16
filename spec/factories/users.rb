a3665acb88e88d0f72d5520e92ef9764c99de419
Alexander Chaschin <chaschin@uchi.ru>
2021-05-16 20:12:34 +0300

    fix(test): user factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,13 +5,16 @@ FactoryBot.define do
     "user#{n}@test.com"
   end
 
-  factory :user do
+  factory :user, aliases: [:student], class: 'Student' do
     email
     password { '123456' }
     password_confirmation { '123456' }
     group
+  end
 
-    factory :admin, class: 'Admin'
-    factory :student, class: 'Student'
+  factory :admin do
+    email
+    password { '123456' }
+    password_confirmation { '123456' }
   end
 end
