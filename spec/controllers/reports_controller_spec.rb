7a28f7f7f6be6e3d7f768b69b39bf7e3648807f1
Alexander Chaschin <chaschin@uchi.ru>
2021-06-01 02:29:27 +0300

    fix: rubocop warning

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -8,8 +8,8 @@ RSpec.describe ReportsController, type: :controller do
   let!(:test) { create(:category, name: 'Test example', target_score: 6) }
   let!(:test_attempt) { create(:test_attempt, category: test, author: student) }
   let!(:question) { create(:question, category: test) }
-  let!(:answer) { create(:static_question, :correct, question: question, test_attempt: test_attempt, author: student) }
-  let!(:answer_wrong) { create(:static_question, :wrong, question: question, test_attempt: test_attempt, author: student) }
+  let(:answer) { create(:static_question, :correct, question: question, test_attempt: test_attempt, author: student) }
+  let(:answer_wrong) { create(:static_question, :wrong, question: question, test_attempt: test_attempt, author: student) }
 
   before { login(student) }
 
