015124086cfd686dc7c85672a37aca80aefbe93a
Alexander Chaschin <chaschin@uchi.ru>
2021-06-05 18:44:26 +0300

    refactor(test): add question with parameters factory

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -6,14 +6,35 @@ FactoryBot.define do
   end
 
   factory :question do
+    transient do
+      scheme_path { 'spec/support/files/397KB.png' }
+    end
+
     formula_text { 'V=I*R' }
     text { 'Найти V' }
     comment
     precision { 0 }
     answer_unit { 'Unit' }
     completion_time { 5 } # seconds
-    scheme { create_file('spec/support/files/397KB.png') }
+    scheme { create_file(scheme_path) }
     test
     association :author, factory: :admin
+
+    factory :question_with_parameters do
+      transient do
+        parameters do
+          {
+            'I' => { minimum: 2, maximum: 50, step: 1, unit: 'А' },
+            'R' => { minimum: 100, maximum: 100_000, step: 100, unit: 'Ом' }
+          }
+        end
+      end
+
+      after(:create) do |question, evaluator|
+        evaluator.parameters.each do |name, info|
+          create(:parameter, question: question, name: name, **info)
+        end
+      end
+    end
   end
 end
