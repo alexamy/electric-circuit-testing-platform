86b7b242655fbac09d23a36aaa2b585fd7e74ea6
Alexander Chaschin <chaschin@uchi.ru>
2021-06-07 14:24:41 +0300

    feat: ignore existing questions

--- spec/tasks/question_seed_spec.rb
+++ spec/tasks/question_seed_spec.rb
@@ -9,12 +9,19 @@ RSpec.describe QuestionSeed, type: :task do
     it 'populate questions' do
       expect do
         described_class.seed
-      end.to change(Question, :count).by(2)
+      end.to change(Question, :count).by_at_least(1)
     end
 
     it 'start with specified index' do
       described_class.seed
       expect(Question.first.id).to eq 1
+    end
+
+    it 'ignores existing questions' do
+      expect do
+        described_class.seed
+        described_class.seed
+      end.not_to raise_error
     end
   end
 end
