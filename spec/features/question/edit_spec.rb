e2b22df4fc1b34028eb729088be6140dc5c94b63
Alexander Chaschin <chaschin@uchi.ru>
2021-06-09 02:03:50 +0300

    refactor(test): group test cases

--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -8,31 +8,31 @@ feature 'Admin can edit question', "
   I'd like to be able to edit question
 " do
   given(:admin) { create(:admin) }
-  given!(:question) do
-    create(:question, formula_text: 'x=y', parameters: %w[y])
-  end
+  given!(:question) { create(:question, formula_text: 'x=y', parameters: %w[y]) }
 
   background { sign_in(admin) }
 
-  scenario 'Admin can edit question fields other than formula' do
-    visit admin_questions_path
+  describe 'Admin' do
+    scenario 'can edit question fields other than formula' do
+      visit admin_questions_path
 
-    click_on 'Редактировать'
-    fill_in 'Текст вопроса', with: 'Sample question text'
-    click_on 'Сохранить вопрос'
+      click_on 'Редактировать'
+      fill_in 'Текст вопроса', with: 'Sample question text'
+      click_on 'Сохранить вопрос'
 
-    expect(page).to have_content 'Вопрос успешно сохранен'
-    expect(page).to have_selector 'td', text: 'Sample question text'
-  end
+      expect(page).to have_content 'Вопрос успешно сохранен'
+      expect(page).to have_selector 'td', text: 'Sample question text'
+    end
 
-  scenario 'Admin can edit question formula' do
-    visit admin_questions_path
+    scenario 'can edit question formula' do
+      visit admin_questions_path
 
-    click_on 'Редактировать'
-    fill_in 'Формула', with: 'x=t'
-    click_on 'Сохранить вопрос'
+      click_on 'Редактировать'
+      fill_in 'Формула', with: 'x=t'
+      click_on 'Сохранить вопрос'
 
-    expect(page).not_to have_field 'Название', with: 'y'
-    expect(page).to have_field 'Название', with: 't'
+      expect(page).not_to have_field 'Название', with: 'y'
+      expect(page).to have_field 'Название', with: 't'
+    end
   end
 end
