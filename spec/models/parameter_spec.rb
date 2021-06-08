0d7e07083b1a5161aa6e279e14a844b43e194419
Alexander Chaschin <chaschin@uchi.ru>
2021-06-08 20:51:42 +0300

    test: group test cases for number types

--- spec/models/parameter_spec.rb
+++ spec/models/parameter_spec.rb
@@ -26,43 +26,56 @@ RSpec.describe Parameter, type: :model do
   describe '#generate_value' do
     before { srand(101) }
 
-    it 'returns minimimum for zero step' do
-      parameter = build(:parameter, minimum: 10, maximum: 100, step: 0)
+    describe 'integer intervals' do
+      it 'returns minimimum for zero step' do
+        parameter = build(:parameter, minimum: 10, maximum: 100, step: 0)
 
-      100.times.each do
-        expect(parameter.generate_value).to eq 10
+        100.times.each do
+          expect(parameter.generate_value).to eq 10
+        end
+      end
+
+      it 'returns minimimum for negative step' do
+        parameter = build(:parameter, minimum: 10, maximum: 100, step: -10)
+
+        100.times.each do
+          expect(parameter.generate_value).to eq 10
+        end
+      end
+
+      it 'returns result greater or equal to minimum' do
+        parameter = build(:parameter, minimum: 10, maximum: 100, step: 3)
+
+        100.times.each do
+          expect(parameter.generate_value).to be >= 10
+        end
+      end
+
+      it 'returns result less than maximum' do
+        parameter = build(:parameter, minimum: 10, maximum: 20, step: 4)
+
+        100.times.each do
+          expect(parameter.generate_value).to be < 20
+        end
+      end
+
+      it 'returns result less than or equal to maximum when maximum is in range' do
+        parameter = build(:parameter, minimum: 10, maximum: 100, step: 3)
+
+        100.times.each do
+          expect(parameter.generate_value).to be <= 100
+        end
       end
     end
 
-    it 'returns minimimum for negative step' do
-      parameter = build(:parameter, minimum: 10, maximum: 100, step: -10)
+    describe 'float intervals' do
+      xit 'returns exact float values' do
+        parameter = build(:parameter, minimum: 0.2, maximum: 0.4, step: 0.1)
 
-      100.times.each do
-        expect(parameter.generate_value).to eq 10
-      end
-    end
-
-    it 'returns result greater or equal to minimum' do
-      parameter = build(:parameter, minimum: 10, maximum: 100, step: 3)
-
-      100.times.each do
-        expect(parameter.generate_value).to be >= 10
-      end
-    end
-
-    it 'returns result less than maximum' do
-      parameter = build(:parameter, minimum: 10, maximum: 20, step: 4)
-
-      100.times.each do
-        expect(parameter.generate_value).to be < 20
-      end
-    end
-
-    it 'returns result less than or equal to maximum when maximum is in range' do
-      parameter = build(:parameter, minimum: 10, maximum: 100, step: 3)
-
-      100.times.each do
-        expect(parameter.generate_value).to be <= 100
+        100.times.each do
+          value = parameter.generate_value
+          expect(value == value.round(1)).to be true
+        end
       end
     end
   end
