d95d6f3c14a05e3e76b6c86e06b86783ce5f38d7
Alexander Chaschin <chaschin@uchi.ru>
2021-06-09 22:29:48 +0300

    feat: add third question

--- lib/tasks/questions/chapter_6.rb
+++ lib/tasks/questions/chapter_6.rb
@@ -50,6 +50,28 @@ module Questions
           'Vcc1' => { minimum: 5, maximum: 12, step: 1, unit: 'В' },
           'Vcc2' => { minimum: -12, maximum: -5, step: 1, unit: 'В' },
         }
+      },
+      3 => {
+        scheme_path: 'lib/assets/schemes/601.png',
+        text: 'Определить напряжение на выходе ОУ (Out)',
+        answer_unit: 'В',
+        precision: 4,
+        completion_time: 60,
+        formula_text: 'Out=clamp(R5*(In1/R1+In2/R2+In3/R3+In4/R4+In5/R5), Vcc2, Vcc1)',
+        parameters: {
+          'In1' => { minimum: 1, maximum: 5, step: 1, unit: 'В' },
+          'In2' => { minimum: 1, maximum: 5, step: 1, unit: 'В' },
+          'In3' => { minimum: 1, maximum: 5, step: 1, unit: 'В' },
+          'In4' => { minimum: 1, maximum: 5, step: 1, unit: 'В' },
+          'In5' => { minimum: 1, maximum: 5, step: 1, unit: 'В' },
+          'R1' => { minimum: 1_000, maximum: 10_000, step: 1_000, unit: 'Ом' },
+          'R2' => { factory: :formula, formula: '2*R1', unit: 'Ом' },
+          'R3' => { factory: :formula, formula: '4*R1', unit: 'Ом' },
+          'R4' => { factory: :formula, formula: '8*R1', unit: 'Ом' },
+          'R5' => { factory: :formula, formula: '16*R1', unit: 'Ом' },
+          'Vcc1' => { minimum: 5, maximum: 12, step: 1, unit: 'В' },
+          'Vcc2' => { minimum: -12, maximum: -5, step: 1, unit: 'В' },
+        }
       }
     }
   end
