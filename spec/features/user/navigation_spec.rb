406cd63633cfc7961cd3ff0af1b861d8f4bca444
Alexander Chaschin <ac@happynumbers.com>
2021-06-25 12:34:03 +0300

    feat: add name or email in navbar

---
+++ spec/features/user/navigation_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'User can see his name', "
+  In order to know what he is logged
+  As an authenticated user
+  I'd like to be able to see my name in navbar
+" do
+  given(:teacher) { create(:admin, email: 'jd.admin@example.com') }
+  given(:student) { create(:student, first_name: 'John', last_name: 'Doe') }
+
+  scenario 'Student see his name' do
+    sign_in(student)
+    
+    within '.navigation-bar' do
+      expect(page).to have_content 'John Doe'
+    end
+  end
+
+  scenario 'Teacher see his email' do
+    sign_in(teacher)
+    
+    within '.navigation-bar' do
+      expect(page).to have_content 'jd.admin@example.com'
+    end
+  end
+end
