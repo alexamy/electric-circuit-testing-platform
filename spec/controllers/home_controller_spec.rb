5781a08ab4d56faca6af1e84c3246132469f7209
Alexander Chaschin <ac@happynumbers.com>
2021-06-25 11:55:24 +0300

    feat: different home redirects

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,9 +4,19 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
   describe 'GET #index' do
-    it 'redirects' do
+    let(:student) { create(:student) }
+
+    it 'redirects to sign in when user is unregistered' do
       get :index
-      expect(response).to redirect_to tests_url
+      
+      expect(response).to redirect_to new_user_session_path
+    end
+
+    it 'redirects to tests path when user is registered' do
+      login(student)
+      get :index
+
+      expect(response).to redirect_to tests_path
     end
   end
 end
